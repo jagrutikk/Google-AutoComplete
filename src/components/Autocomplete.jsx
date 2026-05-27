3413dc3b2f9f2247f272ed507cfa38363c5d31cc
Jagruti Khanage <jkhanage47@gmail.com>
2026-05-27 15:03:40 +0530

    raising pr

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,64 +1,64 @@
 import styles from './Autocomplete.module.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const AutoComplete = () => {
 
-    const[input,setInput] = useState("");
-    const[suggestion,setSuggestion]=useState([]);
+    const [input, setInput] = useState("");
+    const [suggestion, setSuggestion] = useState([]);
 
-    useEffect(()=>{
-        const debouncedSearch = setTimeout(()=>{
-            if(input.length > 1){
-            fetch(`https://api.datamuse.com/sug?s=${input}`)
-            .then((res)=>res.json())
-            .then((data)=>setSuggestion(data));
-        }else{
-            setSuggestion([]);
-        }
-        },300);
-        return() => clearTimeout(debouncedSearch);
-    },[input]);
+    useEffect(() => {
+        const debouncedSearch = setTimeout(() => {
+            if (input.length > 1) {
+                fetch(`https://api.datamuse.com/sug?s=${input}`)
+                    .then((res) => res.json())
+                    .then((data) => setSuggestion(data));
+            } else {
+                setSuggestion([]);
+            }
+        }, 300);
+        return () => clearTimeout(debouncedSearch);
+    }, [input]);
 
-    useEffect(()=>{
+    useEffect(() => {
         const handleClickOutside = (e) => {
-            if(!e.target.closest('#autocomplete')) {
+            if (!e.target.closest('#autocomplete')) {
                 setSuggestion([]);
             }
         };
         document.addEventListener('click', handleClickOutside);
         return () => document.removeEventListener('click', handleClickOutside);
-    },[]);
-    
+    }, []);
 
-    return(
+
+    return (
         <>
-          <input 
-          id="autocomplete"
-          type="text"
-          className={styles.input}
-          placeholder="Search..."
-          onChange={(e)=>setInput(e.target.value)}
-         />
-         {
-            suggestion.length > 0 ? (
-               <div className={styles.suggestionContainer}>
-                 <ul className={styles.ul}>
-                    {suggestion.map((item, index)=>
-                    <>
-                    <li className={styles.li}>{item?.word}</li>
-                    {
-                        suggestion.length - 1 !== index && <hr />
-                    }
-                    </>
-                    )}
-                </ul>
-               </div>
-            ) : (
-               input && (
-                <div>No suggestions</div>
-               )
-            )
-         }
+            <input
+                id="autocomplete"
+                type="text"
+                className={styles.input}
+                placeholder="Search..."
+                onChange={(e) => setInput(e.target.value)}
+            />
+            {
+                suggestion.length > 0 ? (
+                    <div className={styles.suggestionContainer}>
+                        <ul className={styles.ul}>
+                            {suggestion.map((item, index) =>
+                                <>
+                                    <li className={styles.li}>{item?.word}</li>
+                                    {
+                                        suggestion.length - 1 !== index && <hr />
+                                    }
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                ) : (
+                    input && (
+                        <div>No suggestions right now</div>
+                    )
+                )
+            }
         </>
     )
 };
